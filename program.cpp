ca6c45fb09e377e58fe33ea007255b8e56dc947c
RafaelEngel10 <rafaelengelserafim09@gmail.com>
2025-06-09 15:21:59 -0300

    Beta versão 1.0.2

--- program.cpp
+++ program.cpp
@@ -7,6 +7,7 @@ int main() {
     int OP,i,NumerosSorteados[6],NumerosEscolhidos[6],Contador; //variável de opção, variável para função (for), vetor tipo Int para armazenar números
     char Nome[30]; //vetor tipo Char para colocar seu nome
     float Dinheiro; //variável que representa o dinheiro inicial do jogador
+    bool Condicionador = false;   //pra aplicar uma condição de verdade ou falso  
     OP=0; Dinheiro=100; Contador=0;
 
     cout << "Antes de jogar, por favor digite seu nome: ";
@@ -23,12 +24,17 @@ int main() {
        cout << "6. " << endl;
        cout << "7. Roleta Russa (RS50 para jogar)" << endl; 
        cout << "8. Sair" << endl;  
-       cout << "Voce apostou " << Contador << " vezes ate agora." << endl;
+       if (Contador==0) {
+        cout << "Voce apostou nenhuma vez." << endl; 
+       } else if (Contador!=0) {
+        cout << "Voce apostou " << Contador << " vezes ate agora." << endl;
+       }
        cout << "Dinheiro atual: " << Dinheiro << " reais" << endl; 
        cout << "====================================" << endl; 
        cout << "Sua escolha: ";
        cin >> OP;
        switch (OP) {
+
         case 1:
             Dinheiro -= 20;
             cout << "-20 reais." << endl;
@@ -42,35 +48,42 @@ int main() {
                 cout << "Escreva o " << i+1 << "o numero: ";
                 cin >> NumerosEscolhidos[i];
             } 
+            cout << endl;
             for (i=0;i<6;i++) {
                 cout << i+1 << "o numero sorteado: " << NumerosSorteados[i] << " / " << i+1 << "o numero escolhido: " << NumerosEscolhidos[i] << endl;
                 if (NumerosSorteados[i]==NumerosEscolhidos[i]) {
+                    Condicionador = true;
                     cout << endl << "Parabens, voce acertou o numero sorteado, na sua respectiva casa! +150 reais" << endl;
                     cout << endl;
                     Dinheiro = Dinheiro + 150;
-                }
-                for (int j=0;j<6;j++) {
-                    if (NumerosSorteados[i]==NumerosEscolhidos[j]) {
-                    cout << endl <<"Parabens, voce acertou o numero sorteado, mas nao foi na respectiva casa. +50 reais" << endl;
-                    cout << endl;
-                    Dinheiro = Dinheiro + 50; 
-                    } 
+                } else {
+                    for (int j=0;j<6;j++) {
+                    if (NumerosSorteados[i]==NumerosEscolhidos[j]) {                     
+                        Condicionador = true;
+                        cout << endl <<"Parabens, voce acertou o numero sorteado, mas nao foi na respectiva casa. +50 reais" << endl;
+                        cout << endl;
+                        Dinheiro = Dinheiro + 50; 
+                        } 
+                    }
                 }
             }
+            if (Condicionador==false) {      //se as estruturas condicionais não forem executadas, o Condicionador continuará como false, resultando na execução desta condição.
+                cout << "Que pena! Parece que voce nao acertou nem um numero. Talvez na proxima voce consiga!" << endl;
+            }
             Contador += 1;
-        break;
+        break;                   //final case 1
 
         case 2: 
             Dinheiro -= 10;
-
+            
             Contador += 1;
-            break;
+        break;                  //final case 2
 
         case 7: 
             Dinheiro -= 50;
             cout << "Bem vindo a Roleta Russa! Nesse jogo, o programa ira gerar um numero aleatorio de 1 a 6 e voce tem que escolher entre atirar em si mesmo ou atirar no oponente." << endl;
             cout << "Voce começa com 3 vidas, cada vez q tu atirar em si mesmo e for verdadeira, voce perde uma vida e perde a vez, senão voce ganha um vez extra.";
-        break;
+        break;                  //final case 7
        
        default:
         if (OP>8) {
@@ -81,7 +94,8 @@ int main() {
             break;
         }
         break;
-       } 
-    } while (OP!=8);
+       }         //switch fecha-chave
+    } while (OP!=8);          //do fecha-chave
     
-}
+    return 0;
+}        //int main() fecha-chave
